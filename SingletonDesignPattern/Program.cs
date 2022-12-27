ab1ebd8ce1e966fb6913f0e28f4cf109bff144f0
divya-s-simformsolutions <divya.s@simformsolutions.com>
2022-12-27 15:45:32 +0530

    Add project files.

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    public class Program
+    {
+        public void main(string[] args)
+        {
+            SingletonDesignPattern instance1 = SingletonDesignPattern.SingletonDesignPatternInstance;
+            SingletonDesignPattern instance2 = SingletonDesignPattern.SingletonDesignPatternInstance;
+
+            if (instance1 == instance2)
+            {
+                Console.WriteLine("Both objects have the same instance");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
